d931a819f0a28bc98884573599d7168dcc9a19f5
Biswajit Aich <0000biswajitaich@gmail.com>
2026-07-19 21:42:20 +0000

    error:deploy

--- services/ai/config.py
+++ services/ai/config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     HOST: str = "0.0.0.0"
     PORT: int = 8001
 
-    EMBEDDING_PROVIDER: str
-    EMBEDDING_MODEL: str
+
+    # EMBEDDING_PROVIDER: str = "huggingface"
+    EMBEDDING_MODEL: str = "sentence-transformers/all-MiniLM-L6-v2"
 
 settings = Settings()
